6a926979d25868d8a3f75abf6bef2aa2fc76ae40
Hakan Vardar <112097111+HakanVardarr@users.noreply.github.com>
2023-02-10 19:01:46 +0000

    Added documentation for add gate

--- src/gates/and.rs
+++ src/gates/and.rs
@@ -1,5 +1,23 @@
-pub struct And;
 use super::Signal;
+
+/// And Gate
+/// # Usage
+///
+/// ```
+///     use::logic_gate::gates::And;
+///     use::logic_gate::Signal;
+///     
+///     let output = And::send_signal(&Signal::One, &Signal::One);
+///     println!("{output}")
+/// ```
+///
+/// # Truth Table
+/// A B OUT
+/// 1 1 1
+/// 1 0 0
+/// 0 1 0
+/// 0 0 0
+pub struct And;
 
 impl And {
     pub fn send_signal(input1: &Signal, input2: &Signal) -> Signal {
